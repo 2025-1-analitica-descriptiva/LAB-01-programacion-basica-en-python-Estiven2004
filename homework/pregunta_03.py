e815842a279f7a34b90681349cedab93cba49dfc
Estiven <estivenospinajaramillo@gmail.com>
2025-05-09 19:00:15 -0500

    Pregunta 03

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -15,3 +15,18 @@ def pregunta_03():
     [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
 
     """
+    suma_por_letra = {}
+    with open('files/input/data.csv', 'r') as file:
+        for line in file:
+            parts = line.strip().split('\t')
+            if len(parts) >= 2:
+                letra = parts[0]
+                valor = int(parts[1])
+                if letra in suma_por_letra:
+                    suma_por_letra[letra] += valor
+                else:
+                    suma_por_letra[letra] = valor
+
+    lista_ordenada = sorted(suma_por_letra.items())
+
+    return lista_ordenada
